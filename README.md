eeb5aa934d7be68b9c7b391b667ed8522c0dac04
Vitor Moura <vitormoura@infraero.gov.br>
2018-03-25 19:36:37 -0300

    initial configuration of sqlalchemy

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # quotes-service
 REST service to manage motivational or corporate quotes
+
+### Libraries
+
+- FLASK RESTFUL: https://flask-restful.readthedocs.io/en/latest/
+- Flask-SQLAlchemy: http://flask-sqlalchemy.pocoo.org/2.3/quickstart/
+
+### Instalation
+
+```shell
+> pipenv install  
+> FLASK_APP=app.py flask run  
+```
+
--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+db.create_all()
+
+
+#admin = User(username='admin', email='admin@example.com')
+#guest = User(username='guest', email='guest@example.com')
+#db.session.add(admin)
+#db.session.add(guest)
+#db.session.commit()
+
 api = Api(app)
+
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return [u.as_dict() for u in User.query.all()]
 
 api.add_resource(HelloWorld, '/')
 
