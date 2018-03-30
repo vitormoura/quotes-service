5d73a95a0cfabd0d61da6325b80edf90ed07b72e
Vitor Moura <vitormoura@infraero.gov.br>
2018-03-30 10:47:43 -0300

    refactor to support exec using uwsgi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 
 from models import create_db_for_app
@@ -11,19 +11,17 @@ __app = Flask(__name__)
 __app.json_encoder = CustomJSONEncoder
 __app.config.from_json('./appsettings.json', silent=False)
 
+@__app.route('/index.html')
+def index():
+    return __app.send_static_file('index.html')
+
 #Database config
 __db = create_db_for_app(__app, createTables=True)
 
+#API and resources
+api = Api(__app)
+resources.register(api)
 
 def get_db():
     """ get a reference to global app db """
     return __db
-
-
-def run():
-    """ run the flask app (development only) """
-
-    api = Api(__app)
-    resources.register(api)
-        
-    __app.run(debug=True)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import app
+from app import __app as application
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=5000, debug=True)
     
