f58f4feb8057b20a3151fd0847fe89a68a245d34
Vitor Moura <vitormoura@infraero.gov.br>
2018-04-01 10:24:16 -0300

    simple quote creation form implemented

--- static/js/main.js
+++ static/js/main.js
@@ -6,9 +6,17 @@ var ctrl = app.controller('QuotesApp.MainCtrl', function ($scope, $http) {
     function getQuoteCategories() {
         return $http.get('quotes/categories');
     }
-    
+
     const state = {
         categories: [],
+        selectedCategory: null,
+        description: '',
+        author: '',
+        status: {
+            success: true,
+            visible: false,
+            message: ''
+        }
     }
 
     const actions = {
@@ -18,7 +26,48 @@ var ctrl = app.controller('QuotesApp.MainCtrl', function ($scope, $http) {
 
                 actions.selectCategory(state.categories[0])
             })
-        },      
+        },
+
+        selectCategory(c) {
+            state.selectedCategory = c
+        },
+
+        createNewQuote() {
+
+            if ($scope.mainform.$valid) {
+
+                const categ = state.selectedCategory;
+                const desc = state.description
+                const author = state.author;
+
+                const payload = { category_id: categ.id, description: desc, author: author };
+
+                $http.post(`quotes/${categ.acronym}`, payload).then(function (result) {
+                    
+                    state.description = '';
+                    state.author = '';
+                    
+                    $scope.mainform.$setPristine();
+
+                    actions.showStatusMessage('quote successfully created!', true)
+
+                }, function (err) {
+                    actions.showStatusMessage('an internal error occurred. Please try again', false)
+                });
+            }
+        },
+
+        showStatusMessage(msg, isSuccess){
+            state.status.visible = true;
+            state.status.message = msg;
+            state.status.success = isSuccess;
+        },
+
+        closeStatusMessage() {
+            state.status.visible = false;
+        }
+
+
     }
 
     $scope.actions = actions;
